02f98c1d533c8c2c0bfdfb48b8bb6a37f4fe239f
Thomas Bechtold <tbechtold@suse.com>
2016-07-15 11:57:00 +0200

    trove: Fix initial migration and schema revision
    - The schema-revision field was missing in the template schema
    - the attributes passed to the migration are already the attributes
      for the given proposal. So use the correct key when handling the db
      attributes.
      Also check that the db is not already there. Otherwise the password
      would be overwritten.

--- chef/data_bags/crowbar/migrate/trove/001_add_configurable_db_attrs.rb
+++ chef/data_bags/crowbar/migrate/trove/001_add_configurable_db_attrs.rb
@@ -1,12 +1,14 @@
 def upgrade ta, td, a, d
-  a['trove']['db'] = {}
-  a['trove']['db']['password'] = nil
-  a['trove']['db']['user'] = 'trove'
-  a['trove']['db']['database'] = 'trove'
+  unless a["trove"].key? "db"
+    a["trove"]["db"] = {}
+    a["trove"]["db"]["password"] = nil
+    a["trove"]["db"]["user"] = "trove"
+    a["trove"]["db"]["database"] = "trove"
+  end
   return a, d
 end
 
 def downgrade ta, td, a, d
-  a['trove'].delete 'db'
+  a["trove"].delete 'db'
   return a, d
 end
